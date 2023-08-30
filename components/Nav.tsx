e915dfcf39b7661bf9c37fa6ddca56b96de74233
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-30 08:52:41 -0700

    Style sign in and sign up button in Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -31,9 +31,15 @@ export default function Nav() {
       >
         Logo
       </div>
-      <button onClick={() => router.replace('/api/auth/sign-in')}>
-        Sign In
-      </button>
+      <div className='flex gap-5'>
+        <button className='font-semibold hover:text-gray-400 transition-all' onClick={() => router.replace('/api/auth/sign-up')}>Sign Up</button>
+        <button
+          className='bg-blue-950 px-9 py-2 text-white rounded-full font-semibold hover:bg-blue-1000 transition-all'
+          onClick={() => router.replace('/api/auth/sign-in')}
+        >
+          Sign In
+        </button>
+      </div>
     </nav>
   )
 }
