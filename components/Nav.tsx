c1d41e4fe952511bfe75f27bdcc7dbaed09f5aed
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-30 11:34:56 -0700

    Refactor Nav component, prevent buttons flashing

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,40 +5,41 @@ import { useRouter } from 'next/navigation'
 export default function Nav() {
   const { data: session } = useSession()
   const router = useRouter()
-  console.log(session)
-  if (session && session.user) {
-    return (
-      <nav className="flex w-screen justify-between border border-red-600 p-5">
-        <div
-          className='cursor-pointer border border-red-800'
-          onClick={() => router.replace('/')} 
-        >
-          Logo
-        </div>
-        <p>{session.user.name}</p>
-        <button onClick={() => signOut()}>
-          Sign Out
-        </button>
-      </nav>
-    )
-  }
+  console.log('session: ', session)
 
   return (
-    <nav className="flex w-screen justify-between border border-red-600 p-5">
+    <nav style={{ height: '5.5rem' }} className="flex w-screen items-center justify-between border border-red-600 p-5">
       <div
-        className='cursor-pointer border border-red-800'
+        className='cursor-pointer'
         onClick={() => router.replace('/')} 
       >
         Logo
       </div>
       <div className='flex gap-5'>
-        <button className='font-semibold hover:text-gray-400 transition-all' onClick={() => router.replace('/api/auth/sign-up')}>Sign Up</button>
-        <button
-          className='bg-blue-950 px-9 py-2 text-white rounded-full font-semibold hover:bg-blue-1000 transition-all'
-          onClick={() => router.replace('/api/auth/sign-in')}
-        >
-          Sign In
-        </button>
+        {(session && session.user) && (
+          <>
+            <p>{session.user.name}</p>
+            <button onClick={() => signOut()}>
+              Sign Out
+            </button>
+          </>
+        )}
+        {(session === null) && (
+          <>
+            <button
+              className='font-semibold hover:text-gray-400 transition-all'
+              onClick={() => router.replace('/api/auth/sign-up')}
+            >
+              Sign Up
+            </button>
+            <button
+              className='bg-blue-950 px-9 py-2 text-white rounded-full font-semibold hover:bg-blue-1000 transition-all'
+              onClick={() => router.replace('/api/auth/sign-in')}
+            >
+              Sign In
+            </button>
+          </>
+        )}
       </div>
     </nav>
   )
---
+++ components/Separator.tsx
@@ -0,0 +1,9 @@
+const Separator = () => {
+  return (
+    <div className="separator">
+      <span className='mx-2 text-sm text-slate-300'>OR</span>
+    </div>
+  )
+}
+
+export default Separator
