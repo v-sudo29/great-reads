8fbc8822baf6a043a9aefe9d6d0559ab2287253e
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-09-11 08:26:34 -0700

    Change nav links to correct path

--- components/nav/NavLinks.tsx
+++ components/nav/NavLinks.tsx
@@ -45,13 +45,13 @@ const NavLinks = () => {
         <>
           <button
             className='font-semibold hover:text-gray-400 transition-all'
-            onClick={() => router.replace('/api/auth/sign-up')}
+            onClick={() => router.replace('/sign-up')}
           >
             Sign Up
           </button>
           <button
             className='bg-blue-950 px-9 py-2 text-white rounded-full font-semibold hover:bg-blue-1000 transition-all'
-            onClick={() => router.replace('/api/auth/sign-in')}
+            onClick={() => router.replace('/sign-in')}
           >
             Sign In
           </button>
