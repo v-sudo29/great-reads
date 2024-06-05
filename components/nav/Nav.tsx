42f481b763eee4d1401b7d44e18368b129d69cb7
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2024-06-05 13:29:36 -0700

    Restyle nav UI according to Figma design

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,11 +1,21 @@
 import Logo from './Logo'
 import NavLinks from './NavLinks'
 
+const HamburgerNav = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 12H18" stroke="#53675D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M4 6H20" stroke="#53675D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M4 18H20" stroke="#53675D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  )
+}
+
 export default function Nav() {
   return (
-    <nav style={{ height: '5.5rem' }} className="shadow-md flex w-screen items-center justify-between p-5">
-      <Logo/>
-      <NavLinks/>
+    <nav className="h-[3.5rem] flex w-screen items-center justify-between px-5 py-3 border border-b-[#DFE7EB]">
+      <HamburgerNav />
+      <Logo />
     </nav>
   )
 }
