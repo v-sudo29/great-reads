160e0b133fccc870fd8a438e0b9cb40077323392
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2024-06-09 12:07:48 -0700

    Update outline styles to match primary color

--- components/sign-up/SignUpForm.tsx
+++ components/sign-up/SignUpForm.tsx
@@ -24,7 +24,7 @@ const SignUpForm = ({
   const router = useRouter()
 
   const formLabelStyles = 'font-montserrat font-medium text-[14px] text-[#344054] xl:absolute xl:text-[12px] xl:font-semibold xl:text-primary xl:px-[14px] xl:py-[10px] xl:pointer-events-none'
-  const formTextInputStyles = 'border border-[#D0D5DD] rounded-[4px] h-11 px-[14px] py-[10px] placeholder:font-normal placeholder:text-[#A4B1B8] xl:text-[14px] xl:h-[54px] xl:pt-[30px]'
+  const formTextInputStyles = 'border border-[#D0D5DD] rounded-[4px] h-11 px-[14px] py-[10px] focus:outline-primary placeholder:font-normal placeholder:text-[#A4B1B8] xl:text-[14px] xl:h-[54px] xl:pt-[30px]'
   const formInputErrorStyles = formTextInputStyles + ' border-[#D23B2E]'
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
