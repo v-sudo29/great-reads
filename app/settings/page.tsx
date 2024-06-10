55f56a764b8effce9083fff573dcb651be422710
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2024-06-10 14:08:50 -0700

    Create "My Details" section in settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,14 +6,18 @@ import Image from "next/image"
 export default function Settings() {
   const { data: session } = useSession()
   const { imageUrl, loading } = useProfileImage()
-  
+
   if (session === undefined) return <></>
   if (session === null) return <></>
 
+  const firstName = session.user.firstName
+  const lastName = session.user.lastName
+  const email = session.user.email
+
   return (
-    <div className='py-8 px-3 w-full border'>
+    <div className='py-8 px-3 w-full'>
       {/* PROFILE PIC and INFO */}
-      <div className='flex gap-5 items-center'>
+      <div className='flex gap-5 items-center mb-10'>
         <Image
           src={                  
             // If user has imageName -- display imageUrl
@@ -27,11 +31,57 @@ export default function Settings() {
           height='104'
         />
         <div>
-          <p className='font-lora font-bold text-xl text-primary'>
-            {`${session.user.firstName} ${session.user.lastName}` ?? ''}
-          </p>
+          <h1 className='font-lora font-bold text-xl text-primary'>
+            {`${firstName} ${lastName}` ?? ''}
+          </h1>
           <p className='font-montserrat font-medium text-[14px] text-primary'>
             {session.user.email ?? ''}
+          </p>
+        </div>
+      </div>
+
+      {/* MY DETAILS */}
+      <div className='flex flex-col'>
+        <h2 className='font-lora font-bold text-[18px] text-primary'>
+          My Details
+        </h2>
+        <div className='border-b border-b-[#DFE7EB] py-4'>
+          <div className='flex justify-between font-montserrat'>
+            <h3 className='font-bold text-[14px] text-primary leading-[28px]'>
+              First Name
+            </h3>
+            <button className='text-primary font-semibold text-[14px] leading-[28px]'>
+              Edit
+            </button>
+          </div>
+          <p className='font-montserrat font-semibold leading-[24px] text-[#707070]'>
+            {firstName}
+          </p>
+        </div>
+        <div className='border-b border-b-[#DFE7EB] py-4'>
+          <div className='flex justify-between font-montserrat'>
+            <h3 className='font-bold text-[14px] text-primary leading-[28px]'>
+              Last Name
+            </h3>
+            <button className='text-primary font-semibold text-[14px] leading-[28px]'>
+              Edit
+            </button>
+          </div>
+          <p className='font-montserrat font-semibold leading-[24px] text-[#707070]'>
+            {lastName}
+          </p>
+        </div>
+        <div className='border-b border-b-[#DFE7EB] py-4'>
+          <div className='flex justify-between font-montserrat'>
+            <h3 className='font-bold text-[14px] text-primary leading-[28px]'>
+              Email Address
+            </h3>
+            <button className='text-primary font-semibold text-[14px] leading-[28px]'>
+              Edit
+            </button>
+          </div>
+          <p className='font-montserrat font-semibold leading-[24px] text-[#707070]'>
+            {email}
           </p>
         </div>
       </div>
