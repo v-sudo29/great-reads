0e07661155739517bba177a424e8e4caae20b41c
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2024-06-12 15:05:04 -0700

    Update z-index of modal from sign-up page

--- components/sign-in/SignInModal.tsx
+++ components/sign-in/SignInModal.tsx
@@ -5,7 +5,7 @@ import ExitIcon from "@components/common/icons/ExitIcon"
 const SignInModal = () => {
   return (
     <div className='absolute flex justify-center w-full h-full px-4 xl:fixed xl:top-0 xl:left-0'>
-      <div className='relative mt-[6.5rem] flex flex-col w-full h-max max-w-[480px] gap-3 px-4 py-5 bg-white shadow-xl rounded-md xl:mt-auto xl:mb-auto xl:p-6'>
+      <div className='relative mt-[6.5rem] flex flex-col w-full h-max max-w-[480px] gap-3 px-4 py-5 bg-white shadow-xl rounded-xl xl:mt-auto xl:mb-auto xl:p-6'>
         <a 
           href='/'
           className='absolute right-[22px] top-[22px]'
