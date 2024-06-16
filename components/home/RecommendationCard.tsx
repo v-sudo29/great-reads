2e8ec1476f89ce30e9b57f7e0222eea9fdf0aec3
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2024-06-16 10:00:34 -0700

    Create recommendations section, create RecommendationCard component

---
+++ components/home/RecommendationCard.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import FilledStar from '@components/common/icons/FilledStar';
+import UnfilledStar from '@components/common/icons/UnfilledStar';
+
+const truncateText = (string: string, maxCharacters = 32) => {
+  return string.substring(0, 35) + '...';
+};
+
+const RecommendationCard = () => {
+  const truncatedTitle = truncateText(
+    'Chakra Mastery: Unveiling Hidden Potential'
+  );
+
+  return (
+    <div className="flex gap-6">
+      <Image
+        src="/tempUpdateImage1.png"
+        alt=""
+        width="90"
+        height="40"
+        style={{ objectFit: 'cover' }}
+      />
+      <div className="flex flex-col">
+        <p className="font-montserrat font-bold text-primary text-xl leading-[28px]">
+          {truncatedTitle}
+        </p>
+        <p className="font-montserrat font-semibold text-[#707070] leading-[24px] mt-1">
+          by Jiraiya
+        </p>
+        <div className="mt-auto mb-1">
+          <div className="flex gap-1">
+            <FilledStar />
+            <FilledStar />
+            <FilledStar />
+            <FilledStar />
+            <UnfilledStar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecommendationCard;
