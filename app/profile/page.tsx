2d5723665dfffb4f7e8ec71599ab7ce6ba6597ed
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2024-06-17 15:12:23 -0700

    Add tabs border style change logic upon user click

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,12 +2,27 @@
 import { useSession } from 'next-auth/react'
 import ProfilePicture from '@components/settings/ProfilePicture'
 import GreenDotIcon from '@components/common/icons/GreenDotIcon'
+import { useState } from 'react'
+
+const TABS = {
+  FAVORITES: 'Favorites',
+  READ: 'Read',
+  POSTS: 'Posts',
+}
 
 const Profile = () => {
+  const [currentTab, setCurrentTab] = useState(TABS.FAVORITES)
   const { data: session } = useSession()
 
   if (session === undefined) return <></>
   if (session === null) return <></>
+
+  const handleTabClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const buttonElement = e.target as HTMLButtonElement
+    if (buttonElement) setCurrentTab(buttonElement.value)
+  }
 
   return (
     <div className="w-full">
@@ -20,7 +35,7 @@ const Profile = () => {
       </div>
 
       {/* User Details */}
-      <div className="flex flex-col items-center h-[160px] mt-4">
+      <div className="flex flex-col items-center mt-4">
         <h1 className="font-lora font-bold text-xl text-primary leading-[28px]">
           {`${session.user.firstName} ${session.user.lastName}`}
         </h1>
@@ -35,6 +50,52 @@ const Profile = () => {
             &nbsp;Following
           </p>
         </div>
+
+        {/* Tabs */}
+        <div className="w-full mt-10 border-b-2 border-b-[#DFE7EB]">
+          <div className="relative bottom-[-2px] flex w-full justify-center gap-8 font-montserrat font-semibold text-[14px] text-primary ">
+            <button
+              className={
+                currentTab === TABS.FAVORITES
+                  ? 'pb-3 border-b-primary border-b-2'
+                  : 'pb-3 border-b-[#DFE7EB] border-b-2'
+              }
+              onClick={(e) => handleTabClick(e)}
+              value={TABS.FAVORITES}
+            >
+              {TABS.FAVORITES}
+            </button>
+            <button
+              className={
+                currentTab === TABS.READ
+                  ? 'pb-3 border-b-primary border-b-2'
+                  : 'pb-3 border-b-[#DFE7EB] border-b-2'
+              }
+              onClick={handleTabClick}
+              value={TABS.READ}
+            >
+              {TABS.READ}
+            </button>
+            <button
+              className={
+                currentTab === TABS.POSTS
+                  ? 'pb-3 border-b-primary border-b-2'
+                  : 'pb-3 border-b-[#DFE7EB] border-b-2'
+              }
+              onClick={handleTabClick}
+              value={TABS.POSTS}
+            >
+              {TABS.POSTS}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Lists for: Favorites, Read, and Posts */}
+      <div>
+        {currentTab === TABS.FAVORITES && <>Coming Soon</>}
+        {currentTab === TABS.READ && <>Coming Soon</>}
+        {currentTab === TABS.POSTS && <>Coming Soon</>}
       </div>
     </div>
   )
